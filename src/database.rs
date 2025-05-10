18253ea2578970582f0cbf65bd63cd6bbd9ca431
Molly Draven <drazi@duck.com>
2025-05-10 07:21:07 -0400

    Refactor ShardListEntry and AUTH_CREDENTIALS for improved functionality and thread safety

--- src/database.rs
+++ src/database.rs
@@ -2,13 +2,13 @@ use std::sync::{Arc, Mutex};
 use lazy_static::lazy_static;
 use serde::Serialize;
 
-#[derive(Serialize)]
+#[derive(Serialize, Clone)]
 pub struct ShardStatus {
     pub id: u8,
     pub reason: String,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Clone)]
 pub struct ShardListEntry {
     pub name: String,
     pub description: String,
@@ -65,8 +65,57 @@ lazy_static! {
         },
     ]));
 
-    pub static ref AUTH_CREDENTIALS: (String, String) = (
+    pub static ref AUTH_CREDENTIALS: Arc<Mutex<(String, String)>> = Arc::new(Mutex::new((
         "admin".to_string(),
         "password123".to_string(),
-    );
+    )));
+}
+
+impl ShardListEntry {
+    pub fn get_all_shards() -> Vec<ShardListEntry> {
+        let db = SHARD_DATABASE.lock().unwrap();
+        db.clone()
+    }
+
+    pub fn get_shard_by_id(id: u32) -> Option<ShardListEntry> {
+        let db = SHARD_DATABASE.lock().unwrap();
+        db.iter().find(|shard| shard.id == id).cloned()
+    }
+
+    pub fn add_shard(shard: ShardListEntry) {
+        let mut db = SHARD_DATABASE.lock().unwrap();
+        db.push(shard);
+    }
+
+    pub fn update_shard(id: u32, updated_shard: ShardListEntry) -> bool {
+        let mut db = SHARD_DATABASE.lock().unwrap();
+        if let Some(shard) = db.iter_mut().find(|shard| shard.id == id) {
+            *shard = updated_shard;
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn delete_shard(id: u32) -> bool {
+        let mut db = SHARD_DATABASE.lock().unwrap();
+        if let Some(pos) = db.iter().position(|shard| shard.id == id) {
+            db.remove(pos);
+            true
+        } else {
+            false
+        }
+    }
+}
+
+impl AUTH_CREDENTIALS {
+    pub fn get_credentials() -> (String, String) {
+        let creds = AUTH_CREDENTIALS.lock().unwrap();
+        creds.clone()
+    }
+
+    pub fn update_credentials(username: String, password: String) {
+        let mut creds = AUTH_CREDENTIALS.lock().unwrap();
+        *creds = (username, password);
+    }
 }
