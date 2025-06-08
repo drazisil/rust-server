832b056c284fea5249448978c1dcc28e79b2e0e1
Molly Draven <drazi@duck.com>
2025-06-08 16:10:22 -0400

    refactor: replace #pragma once with include guards in http_handlers.hpp

--- src/http_handlers.hpp
+++ src/http_handlers.hpp
@@ -1,5 +1,9 @@
-#pragma once
+#ifndef HTTP_HANDLERS_HPP
+#define HTTP_HANDLERS_HPP
+
 #include <string>
 
 // Handles HTTP requests for the server. Returns true if handled, false otherwise.
 bool handle_http_request(int client_fd, const std::string& request);
+
+#endif // HTTP_HANDLERS_HPP
